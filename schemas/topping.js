4e009f09d9f4cfc37f50751d62fdc582f13faad1
Maënda Fortierë <maenda@gmail.com>
2021-01-03 15:12:39 -0500

    After video 12

---
+++ schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // computer Name
+  name: 'topping',
+  // visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegeterian',
+      type: 'boolean',
+      description: 'Is it vegetarian?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱 ' : ''}`,
+    }),
+  },
+};
